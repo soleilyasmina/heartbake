36a6fecf35fa9051f98f504ec84d597a84e55021
soleilyasmina <mounlasolomon@gmail.com>
2021-06-24 15:18:43 -0400

    add delete button in Snack to make a DELETE request to our API

--- src/components/Snack.jsx
+++ src/components/Snack.jsx
@@ -1,11 +1,24 @@
+import axios from "axios";
+import { baseURL, config } from "../services";
+
 function Snack(props) {
   const { name, description, rating } = props.snack.fields;
+
+  const deleteSnack = async () => {
+    // make our specific URL (???)
+    const snackURL = `${baseURL}/${props.snack.id}`;
+    // make a DELETE request to our specific URL
+    await axios.delete(snackURL, config);
+    // trigger the useEffect ;)
+    props.setToggleFetch((curr) => !curr);
+  }
 
   return (
     <article>
       <h3>{name}</h3>
       <p>{description}</p>
       <p>{rating}</p>
+      <button onClick={deleteSnack}>Delete Snack!</button>
     </article>
   )
 }
