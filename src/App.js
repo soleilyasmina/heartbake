51e22ae8b5d883ea3cd7a586c9432eaf466671a1
soleilyasmina <mounlasolomon@gmail.com>
2021-06-24 13:58:34 -0400

    made api call and stored in snacks state, separated baseURL and config into services folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import { baseURL, config } from "./services";
 import "./App.css";
 
 function App() {
+  const [snacks, setSnacks] = useState([]);
+  const [toggleFetch, setToggleFetch] = useState(true);
+
+  useEffect(() => {
+    const fetchSnacks = async () => {
+      // make our API call to the url with our config
+      const resp = await axios.get(baseURL, config);
+      // console log that data
+      setSnacks(resp.data.records);
+    }
+    fetchSnacks();
+  }, [toggleFetch]); // will only run on mount
+
   return (
     <div className="App">
       <Navbar />
---
+++ src/services/index.js
@@ -0,0 +1,8 @@
+// define our url
+export const baseURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/snacks`;
+// create our config object (sort of like our Airtable passport)
+export const config = {
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+  },
+};
